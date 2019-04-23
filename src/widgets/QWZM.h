6a2878c2ff2a9ac8e1d3ecc96e6e3a4acb711235
inodlite <1023852+inodlite@users.noreply.github.com>
2019-04-23 01:43:56 -0700

    Fixup scaling for connectors and center point.

--- src/widgets/QWZM.h
+++ src/widgets/QWZM.h
@@ -127,7 +127,7 @@ public:
 private:
 	Q_DISABLE_COPY(QWZM)
 	void defaultConstructor();
-	void drawAPoint(const WZMVertex &center, const WZMVertex &color);
+	void drawAPoint(const WZMVertex &center, const WZMVertex &scale, const WZMVertex &color);
 	void drawCenterPoint();
 	void drawNormals();
 	void drawConnectors();
